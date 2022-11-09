e2a09a5a36948635c6af4a02596a6ab9f3465440
unknown <uplnyrandom@gmail.com>
2022-11-09 22:30:21 +0100

    basic test with element drag n' drop

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,134 @@
-<script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
-
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<svelte:head>
+  <title>Example — Add/Remove</title>
+  <meta name="description" content="Svelte-grid — Example — Add/Remove" />
+  <meta name="keywords" content="draggable,resizable,grid,layout,responsive,breakpoints,Svelte,svelte,svelte.js,sveltejs,usage,example,examples,samples,add,remove,dynamic" />
+  <meta name="author" content="Vahe Araqelyan" />
+</svelte:head>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  .remove { cursor: pointer; position: absolute; right: 5px; top: 3px; }
+  :global(.svlt-grid-shadow) {
+    /* Back shadow */
+    background: pink;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+  :global(.svlt-grid-item) {
+    /* Back shadow */
+    background: rgb(206, 13, 45);
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+  :global(.svlt-grid-container) {
+    /* Container color */
+    background: #eee;
   }
-  .read-the-docs {
-    color: #888;
+  :global(.svlt-grid-resizer::after) {
+    /* Resizer color */
+    border-color: white !important;
   }
 </style>
+
+
+<button on:click={add}>Add (random size)</button>
+<button on:click={addAt}>Add random (x=0,y=0)</button>
+<label>
+  <input type="checkbox" bind:checked={adjustAfterRemove} />
+  Adjust elements after removing an item
+</label>
+
+<div class=demo-container>
+  <Grid bind:items={items} rowHeight={100} let:item let:dataItem {cols}>
+    <div class=demo-widget>
+      <span on:pointerdown={e => e.stopPropagation()}
+        on:click={() => remove(dataItem)}
+        class=remove
+        >
+        ✕
+      </span>
+      <p>{dataItem.id}</p>
+    </div>
+  </Grid>
+</div>
+
+<script>
+import Grid from "svelte-grid";
+import gridHelp from "svelte-grid/build/helper/index.mjs";
+// import { openSource } from "./github.js";
+
+const COLS = 6;
+
+const id = () => "_" + Math.random().toString(36).substr(2, 9);
+
+const randomNumberInRange = (min, max) => Math.random() * (max - min) + min;
+
+let items = [
+  {
+    [COLS]: gridHelp.item({
+      x: 0,
+      y: 0,
+      w: 2,
+      h: 2,
+    }),
+    id: id(),
+  },
+
+  {
+    [COLS]: gridHelp.item({
+      x: 2,
+      y: 0,
+      w: 2,
+      h: 2,
+    }),
+    id: id(),
+  },
+];
+
+const cols = [[1100, 6]];
+
+function add() {
+  let newItem = {
+    6: gridHelp.item({
+      w: Math.round(randomNumberInRange(1, 4)),
+      h: Math.round(randomNumberInRange(1, 4)),
+      x: 0,
+      y: 0,
+    }),
+    id: id(),
+  };
+
+  let findOutPosition = gridHelp.findSpace(newItem, items, COLS);
+
+  newItem = {
+    ...newItem,
+    [COLS]: {
+      ...newItem[COLS],
+      ...findOutPosition,
+    },
+  };
+
+  items = [...items, ...[newItem]];
+}
+
+const addAt = () => {
+  let newItem = {
+    6: gridHelp.item({
+      w: Math.round(randomNumberInRange(1, 4)),
+      h: Math.round(randomNumberInRange(1, 4)),
+      x: 0,
+      y: 0,
+    }),
+    id: id(),
+  };
+
+  items = [...[newItem], ...items];
+
+  items = gridHelp.adjust(items, COLS);
+};
+
+const remove = (item) => {
+  items = items.filter((value) => value.id !== item.id);
+
+  if (adjustAfterRemove) {
+    items = gridHelp.adjust(items, COLS);
+  }
+};
+
+let adjustAfterRemove = false;
+</script>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,5 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+declare module 'svelte-grid/build/helper/index.mjs'
+declare module 'svelte-grid'
+declare module 'svelte-grid/utils/helper.js'
