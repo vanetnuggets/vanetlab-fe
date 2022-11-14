672625b07f0cea4d908e78241d4c6c82c811e049
Vojtech Fudaly <fud4ly@gmail.com>
2022-11-14 21:07:41 +0100

    added dynamic creation of containers

--- src/lib/Container.svelte
+++ src/lib/Container.svelte
@@ -1,5 +1,6 @@
 <script>
-    let container = {
+    export let name;
+    export let container = {
         "type": "",
         "data_rate": {
             "value": 0,
@@ -23,41 +24,41 @@
     function createContainer() {
         console.log(container);
     }
-
-    
 </script>
-
-<input bind:value={container.type} placeholder="Type of container">
-
-<form>
-    <p>Data rate: </p>
-    <input type=number bind:value={container.data_rate.value} min=1 max=1024>
-    <select bind:value={container.data_rate.format}>
-        {#each units_rate as unit}
-            <option value={unit}>
-                {unit}
-            </option>
-        {/each}
-    </select>
-</form>
-
-<form>
-    <p>Delay: </p>
-    <input type=number bind:value={container.delay.value} min=1 max=10000>
-    <select bind:value={container.delay.value}>
-        {#each units_delay as unit}
-            <option value={unit}>
-                {unit}
-            </option>
-        {/each}
-    </select>
-</form>
-
-<input bind:value={container.network_address} placeholder="Network adress">
-<input bind:value={container.network_mask} placeholder="Network mask">
-<label>
-    <input type=checkbox bind:checked={container.log_pcap}> Log .pcap
-</label>
-<button on:click={createContainer}>
-    Vytvor kontajner
-</button>
+<div>
+    <p> This is: {name} </p>
+    <input bind:value={container.type} placeholder="Type of container">
+    
+    <form>
+        <p>Data rate: </p>
+        <input type=number bind:value={container.data_rate.value} min=1 max=1024>
+        <select bind:value={container.data_rate.format}>
+            {#each units_rate as unit}
+                <option value={unit}>
+                    {unit}
+                </option>
+            {/each}
+        </select>
+    </form>
+    
+    <form>
+        <p>Delay: </p>
+        <input type=number bind:value={container.delay.value} min=1 max=10000>
+        <select bind:value={container.delay.format}>
+            {#each units_delay as unit}
+                <option value={unit}>
+                    {unit}
+                </option>
+            {/each}
+        </select>
+    </form>
+    
+    <input bind:value={container.network_address} placeholder="Network adress">
+    <input bind:value={container.network_mask} placeholder="Network mask">
+    <label>
+        <input type=checkbox bind:checked={container.log_pcap}> Log .pcap
+    </label>
+    <button on:click={createContainer}>
+        Vytvor kontajner
+    </button>
+</div>
--- src/lib/Topology.svelte
+++ src/lib/Topology.svelte
@@ -1,5 +1,38 @@
 <script>
     import Container from "./Container.svelte";
-</script>
+    
+    let containers = []
+    let data_c = []
+    let name
 
-<Container></Container>
+    let data = {
+        "node_count": 0,
+        "node_containers": containers,
+        "container_settings": {},
+    }
+
+    function addContainer(){
+        containers.push({id: containers.length, name: name})
+        containers = containers;
+        name = ""
+    }
+    function showAll() {
+        console.log(data)
+        containers.forEach(e => {
+            console.log(e.name, data_c[e.id])
+        });
+    }
+</script>
+<button on:click={showAll}>
+    show
+</button>
+
+<input bind:value={name} placeholder="Type container's name">
+<button on:click={addContainer}>
+    Add container
+</button>
+
+{#each containers as c (c.id) }
+    <Container name={c.name} bind:container={data_c[c.id]}/>
+{/each}
+
