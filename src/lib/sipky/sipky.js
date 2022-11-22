8bb53b120d7a6a6d89721887c137c749494c28a1
unknown <uplnyrandom@gmail.com>
2022-11-22 22:05:45 +0100

    working for the first container

--- src/lib/sipky/sipky.js
+++ src/lib/sipky/sipky.js
@@ -1,6 +1,5 @@
 import { nodes, containers } from "../../store/store";
 import LeaderLine from 'leader-line-new';
-import { onMount} from 'svelte'
 
 export default class Sipky {
     
@@ -15,27 +14,33 @@ export default class Sipky {
             });
         });
         containers.subscribe(val => {
-            console.log('++++++++++++++++++++++++++++++++++++++')
+            // array exists and is not empty
             if (Array.isArray(val) && val.length) {
-                // array exists and is not empty
                 this.counter = val[0].nodes.length
                 if (val[0].nodes.length > 1) {
-                    // this.lines.forEach(line => {
-                    //     line.remove()
-                    // });
+                    this.lines.forEach(line => {
+                        line.remove()
+                    });
+                    this.lines = []
                     val[0].nodes.forEach(id1 => {
                         val[0].nodes.slice(-this.counter).forEach(id2 => {
                             if (id1 != id2) {
-                                console.log(id1, id2)
+                                // console.log('arraw btw nodes number: ', id1, id2)
                                 let new_line = new LeaderLine(
                                     document.getElementById(id1.toString()),
-                                    document.getElementById(id2.toString())
-                                );  
+                                    document.getElementById(id2.toString()),
+                                    {path: 'straight'}
+                                );
                                 this.lines.push(new_line)
                             }
                         });
                         this.counter -= 1
                     });
+                }else {
+                    if (this.lines[0] != null) {
+                        this.lines[0].remove()
+                        this.lines = []
+                    }
                 }
             }
         })
