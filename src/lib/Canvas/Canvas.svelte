820388dd041bbc93b7238726195df1a1c72ff4ff
fud4ly <fud4ly@gmail.com>
2023-03-11 21:55:51 +0100

    #️⃣add grid to the canvas background
    Signed-off-by: fud4ly <fud4ly@gmail.com>

--- src/lib/Canvas/Canvas.svelte
+++ src/lib/Canvas/Canvas.svelte
@@ -12,7 +12,7 @@
         circle.attr("cx", event.x).attr("cy", event.y); // move the x/y position
         $nodes[circle.attr("data-id")].x = event.x
         $nodes[circle.attr("data-id")].y = event.y
-        console.log("moving", $nodes[circle.attr("data-id")]);
+        // console.log("moving", $nodes[circle.attr("data-id")]);
     }
     $: dragHandler = drag().on("drag", started); // setup a simple dragHandler
 
@@ -33,7 +33,7 @@
         .on("zoom", handleZoom);
 
     function handleZoom(e) {
-        console.log("ev", e.transform);
+        // console.log("ev", e.transform);
         select(bindHandleZoom).attr("transform", e.transform);
     }
 
@@ -54,7 +54,6 @@
         delay(100).then(() => {
             svg = select(bind)
             dragHandler(svg.selectAll(".myPoint"))
-            console.log(svg)
         });
 	}
 
@@ -65,7 +64,6 @@
     onMount(() => {
         svg = select(bind)
         dragHandler(svg.selectAll(".myPoint"))
-        console.log(svg)
     })
 
 </script>
@@ -77,7 +75,16 @@
 </div>
 
 <svg bind:this={bind} height="97%" width="100%">
+    
     <g bind:this={bindHandleZoom}>
+        <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+                <pattern id="smallGrid" width="8" height="8" patternUnits="userSpaceOnUse">
+                    <path d="M 8 0 L 0 0 0 8" fill="none" stroke="gray" stroke-width="0.5"/>
+                </pattern>
+            </defs>
+            <rect width="100%" height="100%" fill="url(#smallGrid)" />
+        </svg>
         {#each $nodes as d, i}
         <circle class="myPoint"
             data-id={d.id} cx={d.x} cy={d.y} r="15" fill={colors[d.net]}/>
