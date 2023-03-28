62dcdb1f23772354c8257079395f30bef1b4fa43
unknown <andrejdubak@gmail.com>
2023-03-28 16:35:18 +0200

    refactor configuration of aplication + add new conf values

--- src/lib/topology/L3.svelte
+++ src/lib/topology/L3.svelte
@@ -1,45 +1,88 @@
 <script>
-    import '../../assets/nodeconf.css'
+    import "../../assets/nodeconf.css";
     import { slide } from "svelte/transition";
     import { nodes } from "../../store/scenario";
 
     export let node_id;
 
     const l3_types = {
-        udpclient : {
+        udpclient: {
+            port: "1",
+            start: "0",
+            stop: "0",
+            interval: "0",
+            packet_size: "0",
+            max_packets: "0",
             comm: null,
-            port: 1,
-            start: 0,
-            stop: 0,
-            max_packets: 0,
         },
         udpserver: {
-            port: 1,
-            start: 0,
-            stop: 0,
+            port: "1",
+            start: "0",
+            stop: "0",
         },
         tcpclient: {
-            port: 1,
-            start: 0,
-            stop: 0,
+            port: "1",
+            start: "0",
+            stop: "0",
+            max_bytes: "0",
+            comm: null,
         },
-        tcpserver: {   
-            port: 1,
-            start: 0,
-            stop: 0,
-        }
-    }
+        tcpserver: {
+            port: "1",
+            start: "0",
+            stop: "0",
+        },
+    };
+
+    const validation_attributes = {
+        port: {
+            name: "Port",
+            placeholder: "Port from range 1-65535",
+            validation: 1,
+        },
+        start: { name: "Start", placeholder: "unsigned int", validation: 2 },
+        stop: {
+            name: "Stop",
+            placeholder: "unsigned int > start",
+            validation: 3,
+        },
+        comm: {
+            name: "Communication",
+            placeholder: "Pick nodes to communicate with",
+            validation: 0,
+        },
+        interval: {
+            name: "Interval",
+            placeholder: "unsigned int",
+            validation: 2,
+        },
+        packet_size: {
+            name: "Packet size",
+            placeholder: "unsigned int",
+            validation: 2,
+        },
+        max_packets: {
+            name: "Max packets",
+            placeholder: "unsigned int",
+            validation: 2,
+        },
+        max_bytes: {
+            name: "Max bytes",
+            placeholder: "unsigned int",
+            validation: 2,
+        },
+    };
 
     let open_l3 = false;
     function toggle_l3() {
         open_l3 = !open_l3;
     }
-  
-    function reset_l3conf() {
-        $nodes[node_id].l3conf = l3_types[$nodes[node_id].l3]
-        $nodes = $nodes
-    }
 
+    function reset_l3conf() {
+        $nodes[node_id].l3conf = l3_types[$nodes[node_id].l3];
+
+        $nodes = $nodes;
+    }
 </script>
 
 <div class="L3">
@@ -53,7 +96,10 @@
                     Application: <br />
                 </div>
                 <div class="col">
-                    <select bind:value={$nodes[node_id].l3} on:change={reset_l3conf}>
+                    <select
+                        bind:value={$nodes[node_id].l3}
+                        on:change={reset_l3conf}
+                    >
                         {#each Object.keys(l3_types) as l3_typ}
                             <option value={l3_typ}>
                                 {l3_typ}
@@ -64,63 +110,15 @@
             </div>
             {#if $nodes[node_id].l3 != null}
                 <div transition:slide>
-                    <div class="row">
-                        <div class="col">
-                            Port: <br />
-                        </div>
-                        <div class="col">
-                            <input
-                                class="my-input"
-                                bind:value={$nodes[node_id].l3conf.port}
-                                placeholder="Port from range 1-65535"
-                            />
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            Start: <br />
-                        </div>
-                        <div class="col">
-                            <input
-                            class="my-input"
-                            bind:value={$nodes[node_id].l3conf.start}
-                            placeholder="unsigned int"
-                            />
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            Stop: <br />
-                        </div>
-                        <div class="col">
-                            <input
-                            class="my-input"
-                            bind:value={$nodes[node_id].l3conf.stop}
-                            placeholder="unsigned int > start"
-                            />
-                        </div>
-                    </div>
-                    {#if $nodes[node_id].l3 == "udpclient"}
-                        <div transition:slide>
-                            <div class="row">
-                                <div class="col">
-                                    Max Packet: <br />
-                                </div>
-                                <div class="col">
-                                    <input
-                                        class="my-input"
-                                        bind:value={$nodes[node_id].l3conf.max_packets}
-                                        placeholder="max packet"
-                                    />
-                                </div>
+                    {#each Object.keys($nodes[node_id].l3conf) as key}
+                        <div class="row">
+                            <div class="col">
+                                {validation_attributes[key].name}: <br />
                             </div>
-                            <div class="row">
-                                <div class="col">
-                                    Communication: <br />
-                                </div>
-                                <div class="col">
+                            <div class="col">
+                                {#if validation_attributes[key].validation === 0}
                                     <select
-                                        bind:value={$nodes[node_id].l3conf.comm}
+                                        bind:value={$nodes[node_id].l3conf[key]}
                                     >
                                         {#each Object.keys($nodes) as node}
                                             <option value={node}>
@@ -128,10 +126,17 @@
                                             </option>
                                         {/each}
                                     </select>
-                                </div>
+                                {:else}
+                                    <input
+                                        class="my-input"
+                                        bind:value={$nodes[node_id].l3conf[key]}
+                                        placeholder={validation_attributes[key]
+                                            .placeholder}
+                                    />
+                                {/if}
                             </div>
                         </div>
-                    {/if}
+                    {/each}
                 </div>
             {/if}
         </div>
