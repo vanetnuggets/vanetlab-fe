14d9e3df487606bd92de975b430b1aeac5a69b9e
Vojtech Fudaly <fud4ly@gmail.com>
2023-04-21 18:06:33 +0200

    brrrrr

--- src/lib/layouts/Logo.svelte
+++ src/lib/layouts/Logo.svelte
@@ -1,10 +1,60 @@
 
 <div class="data-icon">
-    <img src={logo}  height=64px width=64px alt="map_icon">
+  <svg
+   width="121.526mm"
+   height="59.060001mm"
+   viewBox="0 0 121.526 59.060002"
+   version="1.1"
+   id="svg5"
+   xmlns:xlink="http://www.w3.org/1999/xlink"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <defs
+     id="defs2">
+    <rect
+       x="87.172974"
+       y="198.48616"
+       width="439.21768"
+       height="280.29465"
+       id="rect3319" />
+    <rect
+       x="138.8062"
+       y="227.3203"
+       width="367.46762"
+       height="91.866905"
+       id="rect2535" />
+  </defs>
+  <g
+     id="layer1"
+     transform="translate(-21.644536,-74.580717)">
+    <path
+       style="fill:none;stroke:none;stroke-width:0.265;stroke-dasharray:none;stroke-opacity:1"
+       d="m 45.042991,122.26346 c 42.580646,-48.258064 80.548399,0 80.548399,0"
+       id="path1019" />
+    <text
+       xml:space="preserve"
+       id="text2533"
+       style="font-size:25px;letter-spacing:1.6219px;white-space:pre;fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:1.00157;stroke-dasharray:none;stroke-opacity:1"><textPath
+         xlink:href="#path1019"
+         id="textPath4775">VANETLAB</textPath></text>
+    <text
+       xml:space="preserve"
+       transform="matrix(0.26458333,0,0,0.26458333,5.7446044,4.4570084)"
+       id="text3317"
+       style="font-size:80.6667px;white-space:pre;shape-inside:url(#rect3319);display:inline;fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:1.00157;stroke-dasharray:none;stroke-opacity:1" />
+    <path
+       style="fill:none;fill-opacity:1;stroke:none;stroke-width:0.264999;stroke-dasharray:none;stroke-opacity:1"
+       d="m 34.15388,113.32597 c -8.126813,-3.15037 27.813679,-30.950231 47.969752,-28.852834 10.079433,1.048844 31.584148,-0.06847 22.038438,12.039614 -4.87873,6.18834 1.42687,6.88185 4.61153,3.9723 11.06055,-10.105103 7.94755,-7.475393 22.85004,5.54454 2.91386,4.31047 -0.64249,6.83078 -1.69114,7.53375 -0.91834,0.61563 -2.08848,13.7003 -15.20806,-0.0156 -16.469775,-11.73902 -20.697799,-12.20223 -32.955642,-10.80041 -11.856517,1.35591 -10.683377,-0.68837 -27.189516,12.99597 -8.411109,6.97319 -4.22654,3.86219 -20.425402,-2.41733 z"
+       id="path4777" />
+    <image id="car" xlink:href={car} width="10px" height="10px" x="-15px" y="-5px">
+      <animateMotion xlink:href="#car" dur="20s" repeatCount="indefinite" rotate="auto"><mpath xlink:href="#path4777"/></animateMotion>
+    </image>
+  </g>
+</svg>
 </div>
 
 
 <script>
-  import logo from '../../assets/logo.svg';
+  import car from '../../assets/logo.svg';
 
 </script>
