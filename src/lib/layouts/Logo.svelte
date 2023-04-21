40d553118e74aadbf990ef858b76173f6143abbb
Vojtech Fudaly <fud4ly@gmail.com>
2023-04-21 17:21:44 +0200

    place for a logo

---
+++ src/lib/layouts/Logo.svelte
@@ -0,0 +1,10 @@
+
+<div class="data-icon">
+    <img src={logo}  height=64px width=64px alt="map_icon">
+</div>
+
+
+<script>
+  import logo from '../../assets/logo.svg';
+
+</script>
--- src/lib/layouts/Welcome.svelte
+++ src/lib/layouts/Welcome.svelte
@@ -1,56 +1,56 @@
-<div class="float">
-  ahoj
+<div class="logo">
+  <Logo/>
 </div>
 
 <div class="flex-container">
-  <div class="flex-item ">
-    <div class="center-container">
-      VanetLab
-      <div class="inside">
-        <div class="row">  
-          <button on:click={click_new_sim} class="btn-basic">New simulation</button>
-        </div>
-        <div class="row">  
-          <button on:click={click_load_sumo} class="btn-basic">Load from sumo</button>
-        </div>
-        <div class="row">  
-          <button on:click={click_load_local} class="btn-basic">Load local</button>
-        </div>
-        <div class="row">  
-          <button on:click={click_load_remote} class="btn-basic">Load remote</button>
-        </div>
-        <div class="row bottom" style="padding-left: 10px;">
-          <p style="padding-left: 10px; text-align: justify; width: 30%;">Api key:</p>
-          <input id="api-key-input" placeholder="api key" bind:value={apiSecret} style="width: 100%;"/>
-          <button on:click={setApiKey} class="">set</button>
+    <div class="flex-item ">
+      <div class="center-container">
+        VanetLab
+        <div class="inside">
+          <div class="row">  
+            <button on:click={click_new_sim} class="btn-basic">New simulation</button>
+          </div>
+          <div class="row">  
+            <button on:click={click_load_sumo} class="btn-basic">Load from sumo</button>
+          </div>
+          <div class="row">  
+            <button on:click={click_load_local} class="btn-basic">Load local</button>
+          </div>
+          <div class="row">  
+            <button on:click={click_load_remote} class="btn-basic">Load remote</button>
+          </div>
+          <div class="row bottom" style="padding-left: 10px;">
+            <p style="padding-left: 10px; text-align: justify; width: 30%;">Api key:</p>
+            <input id="api-key-input" placeholder="api key" bind:value={apiSecret} style="width: 100%;"/>
+            <button on:click={setApiKey} class="">set</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
 
-  {#if curr_option!=null}
-  <div class="flex-item">
-    <div class="center-container">
-      <div class="inside">
-        {#if curr_option == 'NEW'}
-          <NewSim></NewSim>
-        {/if}
-        {#if curr_option == 'SUMO'}
-          <FromSumo></FromSumo>
-        {/if}
-        {#if curr_option == 'LOCAL'}
-        <div>
-          <LoadLocal></LoadLocal>
+    {#if curr_option!=null}
+    <div class="flex-item">
+      <div class="center-container">
+        <div class="inside">
+          {#if curr_option == 'NEW'}
+            <NewSim></NewSim>
+          {/if}
+          {#if curr_option == 'SUMO'}
+            <FromSumo></FromSumo>
+          {/if}
+          {#if curr_option == 'LOCAL'}
+          <div>
+            <LoadLocal></LoadLocal>
+          </div>
+          {/if}
+          {#if curr_option == 'REMOTE'}
+          <div>
+            <LoadRemote bind:this={loadRemoteComponent}></LoadRemote>
+          </div>
+          {/if}
         </div>
-        {/if}
-        {#if curr_option == 'REMOTE'}
-        <div>
-          <LoadRemote bind:this={loadRemoteComponent}></LoadRemote>
-        </div>
-        {/if}
       </div>
     </div>
-  </div>
   {/if}
 </div>
 
@@ -98,10 +98,21 @@
   transition: opacity 0.4s, transform 1s;
   transition: 0.3s;
   justify-content: space-evenly;
-  height: 100vh;
+  height: 70vh;
 }
 .flex-item {
-  
+  margin-top: -30vh;
+}
+
+.logo {
+  background-color: red;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  transition: opacity 0.4s, transform 1s;
+  transition: 0.3s;
+  justify-content: space-evenly;
+  height: 30vh;
 }
 </style>
 
@@ -110,9 +121,12 @@
   import FromSumo from "./welcome/FromSumo.svelte";
   import LoadRemote from "./welcome/LoadRemote.svelte";
   import LoadLocal from "./welcome/LoadLocal.svelte";
+  import Logo from "./Logo.svelte"
 
   import { listScenarios } from "../api/scenarios";
   import { scenarioList } from "../../store/welcome";
+
+
 
   let curr_option = null
   let loadRemoteComponent;
