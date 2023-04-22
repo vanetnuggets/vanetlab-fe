4bfe762184d8e65e63a7f99002a08454774e3b14
unknown <uplnyrandom@gmail.com>
2023-04-22 18:47:38 +0200

    fixed exit error

--- src/lib/layouts/RightDrawer.svelte
+++ src/lib/layouts/RightDrawer.svelte
@@ -1,11 +1,5 @@
 <script>
   import NodeInfo from "../topology/NodeInfo.svelte";
-  import { show_rdrawer } from '../../store/store.js';
-
-  let elem_rdrawer;
-  show_rdrawer.subscribe(value => {
-    elem_rdrawer = value;
-  })
 </script>
 
 <div class=r_drawer>
