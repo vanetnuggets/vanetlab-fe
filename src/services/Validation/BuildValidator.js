e2f054b04bda4e6204b6b943679b0dfca45d4280
unknown <andrejdubak@gmail.com>
2023-04-26 16:45:34 +0200

    new type of validation

---
+++ src/services/Validation/BuildValidator.js
@@ -0,0 +1,16 @@
+function buildValidator (validators) {
+    return function validate (value) {
+      if (!validators || validators.length === 0) {
+        return { valid: true }
+      }
+  
+      const failing = validators.find(v => v(value) !== true)
+  
+      return {
+        valid: !failing,
+        message: failing && failing(value)
+      }
+    }
+  }
+  
+  export { buildValidator }
---
+++ src/services/Validation/Validators.js
@@ -0,0 +1,30 @@
+function emailValidator () {
+    return function email (value) {
+      return (value && !!value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) || 'Please enter a valid email'
+    }
+  }
+  
+function requiredValidator () {
+    return function required (value) {
+      return (value !== undefined && value !== null && value !== '') || 'This field is required'
+    }
+  }
+
+  
+function integerValidation(value, form) {
+    if (!Number.isInteger(Number(value)) || Number(value) < 0)
+		return { integerValidation: true } 
+}
+
+function floatValidation(value, form) {
+    if (Number.isNaN(Number(value)) || Number(value) < 0)
+		return { floatValidation: true } 
+}
+  
+  export {
+    emailValidator,
+    requiredValidator,
+    integerValidation,
+    floatValidation,
+  }
+  
