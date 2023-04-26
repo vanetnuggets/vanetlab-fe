73835195d5f6bb18de67124dc3e8c3e65343ef12
omartinka <ondrejmar23@gmail.com>
2023-04-26 21:49:36 +0200

    cakam na pusu 😘

--- src/services/Validation/Validators.js
+++ src/services/Validation/Validators.js
@@ -1,15 +1,24 @@
 function emailValidator () {
-    return function email (value) {
-      return (value && !!value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) || 'Please enter a valid email'
-    }
+  return function email (value) {
+    return (value && !!value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) || 'Please enter a valid email'
   }
-  
-function requiredValidator () {
-    return function required (value) {
-      return (value !== undefined && value !== null && value !== '') || 'This field is required'
-    }
-  }
+}
 
+function requiredValidator () {
+  return function required (value) {
+    return (value !== undefined && value !== null && value !== '') || 'This field is required'
+  }
+}
+
+function positiveFloatValidator () {
+  return function float (value) {
+    if (parseFloat(value) >= 0) {
+      return true;
+    } else {
+      return 'Please enter a positive floating point number';
+    }
+  }
+}
   
 function integerValidation(value, form) {
     if (!Number.isInteger(Number(value)) || Number(value) < 0)
@@ -26,5 +35,6 @@ function floatValidation(value, form) {
     requiredValidator,
     integerValidation,
     floatValidation,
+    positiveFloatValidator,
   }
   
