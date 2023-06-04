f62c012af1dec4058a5101bc94aeb9c3a966d014
SamK <samotion2@gmail.com>
2023-06-04 15:58:53 +0200

    input name check

--- src/lib/layouts/welcome/LoadLocal.svelte
+++ src/lib/layouts/welcome/LoadLocal.svelte
@@ -12,8 +12,11 @@
   let simName = "";
 
   async function loadSim() {
+    // check if the inputed simname is in correct format
+    if (checkSimName() == -1)
+      return
+    
     let content = await simFile.text();
-
     scenarioName.set(simName);
     loadConfig(JSON.parse(content));
     push(`/app/${simName}/canvas`);
@@ -60,6 +63,28 @@
     } else {
       simFile = e.detail.acceptedFiles[0];
     }
+  }
+
+  function checkSimName() {
+    var pattern = /^[A-Za-z0-9-_]+$/;
+    if (simName == "") {
+      addNotification({
+            text: `You have to specify scenario name!`,
+            position: "bottom-center",
+            type: "error",
+            removeAfter: 5000,
+          });
+      return -1
+    } else if (!pattern.test(simName)) {
+      addNotification({
+            text: `Scenario name has to match this regex ${pattern}!`,
+            position: "bottom-center",
+            type: "error",
+            removeAfter: 5000,
+          });
+      return -1
+    }
+    return 1
   }
 </script>
 
